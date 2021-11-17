ff69dc489d7f150db3652620b5b2cdd37046cf05
ibrahim <ib.arshad777@gmail.com>
2021-11-17 22:56:06 +0600

    Migrate to .Net 6

--- Program.cs
+++ Program.cs
@@ -1,17 +1,10 @@
-﻿namespace ConsoleAppHelloWorld
-{
-    class Program
-    {
-        static void Main()
-        {
-            // App.HelloWorld.AppMain.Run();
-            // App.FindDuplicateFile.AppMain.Run();
-            // App.CsharpClassTest.AppMain.Run();
-            // App.InstanceWithActivator.AppMain.Run();
-            // App.NewVsOverride.AppMain.Run();
-            // App.MagicOfLinq.AppMain.Run();
-            // App.DeligatesAreDelicate.AppMain.Run();
-            App.ExcelGenerate.AppMain.Run();
-        }
-    }
-}
+﻿using App = ConsoleAppHelloWorld.App;
+
+// App.HelloWorld.AppMain.Run();
+// App.FindDuplicateFile.AppMain.Run();
+// App.CsharpClassTest.AppMain.Run();
+// App.InstanceWithActivator.AppMain.Run();
+// App.NewVsOverride.AppMain.Run();
+// App.MagicOfLinq.AppMain.Run();
+// App.DeligatesAreDelicate.AppMain.Run();
+App.ExcelGenerate.AppMain.Run();
